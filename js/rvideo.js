5d208af985d0ebfad2319875f44d2fbf8d16b533
kernelroni <kernelroni@yahoo.com>
2018-02-02 15:49:01 +0600

    add fontawsome for controlls

--- js/rvideo.js
+++ js/rvideo.js
@@ -4,61 +4,39 @@ function rvideolog(msg){
 	
 }
 
-window.onload = function(){
-	
-	return;
-	//rvideolog(rvideo);
-	
-	var videoTag = document.getElementById(rvideo_id);
-
-	
-    if (rvideo.canPlayType("video/mp4")) {
-        rvideo.setAttribute("src","videos/mov_bbb.mp4");
-    } else {
-        rvideo.setAttribute("src","videos/mov_bbb.ogg");
-    }
-
-
-	rvideolog(videoTag);
-	
-	
-    videoTag.setAttribute("width", "320");
-    videoTag.setAttribute("height", "240");
-    videoTag.setAttribute("controls", "controls");
-    //document.getElementById(rvideo_wrapper_id).appendChild(rvideo);
-
-	
-};
-
 
 
 (function($) {
 
-    $.fn.html5rvedio = function( options ) {
+    $.fn.rvideo = function( options ) {
 
         // Establish our default settings
         var settings = $.extend({
-            mp4         : 'videos/mov_bbb.mp4',
-            ogg         : 'videos/mov_bbb.ogg',
-            width    	: 0,
-			height		: 0,
+            mp4url         : 'videos/mov_bbb.mp4',
+            oggurl         : 'videos/mov_bbb.ogg',
+            width    	: 320,
+			height		: 240,
 			playlist	: [],
 			playlist_container_id : "rvideo-playlist",
 			
 			
         }, options);
 
-        return this.each( function() {            
+        return this.each( function(i,wrapper) {            
 			// the selector elements
-			var rvideo = this;
+			rvideolog(wrapper);
+			var rplayer = jQuery(wrapper).children("video")[0];
 			
-			if (rvideo.canPlayType("video/mp4")) {
-				rvideo.setAttribute("src",settings.mp4);
+			rvideolog(rplayer);
+			
+			if (rplayer.canPlayType("video/mp4")) {
+				rplayer.setAttribute("src",settings.mp4url);
 			} else {
-				rvideo.setAttribute("src",settings.ogg);
+				rplayer.setAttribute("src",settings.oggurl);
 			}			
+
 			
-			rvideo.setAttribute("controls", "controls");
+			rplayer.setAttribute("controls", "controls");
 			
 			
         });
