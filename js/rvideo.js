6a467686200d1c4df6a9086ba5ad5740df9472c3
kernelroni <kernelroni@yahoo.com>
2018-02-19 14:36:22 +0600

    update control bar icons

--- js/rvideo.js
+++ js/rvideo.js
@@ -69,7 +69,7 @@ function rvideolog(msg){
 				rplayer.setAttribute("src",settings.oggurl);
 			}			
 
-			// show the player default controlls - provided by the browser.
+			// show the player default controls - provided by the browser.
 			if(settings.default_controls){
 				rplayer.setAttribute("controls", "controls");
 			}else if(!settings.default_controls){
@@ -93,6 +93,15 @@ function rvideolog(msg){
         	controlBar.setAttribute("id","rvideo-controlbar");
         	controlBar.setAttribute("class","rvideo-controlbar");
 
+        	var controlBarLeftPanel = document.createElement("div");
+        	controlBarLeftPanel.setAttribute("id","rvideo-controlBarLeftPanel");
+        	controlBarLeftPanel.setAttribute("class","rvideo-controlBarLeftPanel");
+
+        	var controlBarRightPanel = document.createElement("div");
+        	controlBarRightPanel.setAttribute("id","rvideo-controlBarRightPanel");
+        	controlBarRightPanel.setAttribute("class","rvideo-controlBarRightPanel");
+
+
         	if(settings.control_bar_position == "top"){
         		controlBar.style.top = "0px";        		
         	}else{
@@ -100,38 +109,143 @@ function rvideolog(msg){
         	}
 
 
-
+        	// add play pause buttons
         	if(settings.controls.play){
+
+        	window.playPaushWrapper = document.createElement("div"); // play pause button wrapper
+			playPaushWrapper.setAttribute("id","rvideo-playPaushWrapper");
+        	playPaushWrapper.setAttribute("class","rleft rvideo-playPaushWrapper");        	
 
         	window.playButton = document.createElement("div"); // global play button
         	window.pauseButton = document.createElement("div"); // global pause button
 
         	playButton.setAttribute("id","rvideo-playbutton");
         	playButton.setAttribute("class","rcontrolbutton rvideo-playbutton");
-        	playButton.innerHTML = "<i class='fa fa-play'></i>";
+        	playButton.innerHTML = "<i class='fas fa-play'></i>";
 
         	pauseButton.setAttribute("id","rvideo-pausebutton");
         	pauseButton.setAttribute("class","rcontrolbutton rvideo-pausebutton");
-        	pauseButton.innerHTML = "<i class='fa fa-pause'></i>";
+        	pauseButton.innerHTML = "<i class='fas fa-pause'></i>";
 
 
-        	playButton.addEventListener("click", onPlayButtonClick);
-        	pauseButton.addEventListener("click", onPauseButtonClick);
+
         	pauseButton.style.display = "none"; // hide pause button by default
 
-        	controlBar.appendChild(playButton);
-        	controlBar.appendChild(pauseButton);
+
+        	playPaushWrapper.appendChild(playButton);
+        	playPaushWrapper.appendChild(pauseButton);
+
+			controlBarLeftPanel.appendChild(playPaushWrapper);
+        	
+
+        	} // // play pause buttons  end
+
+
+        	// add volume buttons
+        	if(settings.controls.volume){
+
+
+        	window.rvideoVolumeWrapper = document.createElement("div");
+			rvideoVolumeWrapper.setAttribute("id","rvideo-rvideoVolumeWrapper");
+        	rvideoVolumeWrapper.setAttribute("class","rleft rvideo-rvideoVolumeWrapper");
+
+        	window.volumeplusminusWrapper = document.createElement("div"); // play pause button wrapper
+			volumeplusminusWrapper.setAttribute("id","rvideo-volumeplusminusWrapper");
+        	volumeplusminusWrapper.setAttribute("class","rleft rvideo-volumeplusminusWrapper");     
+
+
+        	window.volumeWrapper = document.createElement("div");
+			volumeWrapper.setAttribute("id","rvideo-volumewrapper");
+        	volumeWrapper.setAttribute("class","rleft rvideo-volumewrapper");   
+
+
+
+
+
+        	window.volumeButton = document.createElement("div");
+        	window.volumeMute = document.createElement("div") ;
+        	window.plusButton = document.createElement("div"); 
+        	window.minusButton = document.createElement("div"); 
+
+
+        	volumeButton.setAttribute("id","rvideo-volumeButton");
+        	volumeButton.setAttribute("class","rcontrolbutton rleft rvideo-volumebutton");
+        	volumeButton.innerHTML = "<i class='fas fa-volume-up'></i>";
+
+        	volumeMute.setAttribute("id","rvideo-volumeMute");
+        	volumeMute.setAttribute("class","rcontrolbutton rleft rvideo-volumemutebutton");
+        	volumeMute.innerHTML = "<i class='fas fa-volume-off'></i>";
+
+        	minusButton.setAttribute("id","rvideo-minusButton");
+        	minusButton.setAttribute("class","rcontrolbutton rleft rvideo-volumebutton");
+        	minusButton.innerHTML = "<i class='fas fa-minus-square'></i>";
+
+        	plusButton.setAttribute("id","rvideo-plusButton");
+        	plusButton.setAttribute("class","rcontrolbutton rleft rvideo-volumemutebutton");
+        	plusButton.innerHTML = "<i class='fas fa-plus-square'></i>";
+
+			volumeplusminusWrapper.appendChild(minusButton); 
+        	volumeplusminusWrapper.appendChild(plusButton); 
+        	
+
+
+        	volumeWrapper.appendChild(volumeButton); 
+        	volumeWrapper.appendChild(volumeMute);         	       	
+
+        	rvideoVolumeWrapper.appendChild(volumeWrapper);
+        	rvideoVolumeWrapper.appendChild(volumeplusminusWrapper);
+
+
+        	controlBarLeftPanel.appendChild(rvideoVolumeWrapper);
+
+
+        	}        	
+
+
+
+			// add fullscreen button
+        	if(settings.controls.fullscreen){
+
+        	window.fullscreenButton = document.createElement("div");
+			fullscreenButton.setAttribute("id","rvideo-fullscreenButton");
+        	fullscreenButton.setAttribute("class","rcontrolbutton rright rvideo-fullscreenButton");
+        	fullscreenButton.innerHTML = "<i class='fas fa-expand-arrows-alt'></i>"; 
+
+
+			controlBarRightPanel.appendChild(fullscreenButton);
 
         	}
 
 
+        	// add left control panel
+        	controlBar.appendChild(controlBarLeftPanel);
+
+        	// add right control panel 
+			controlBar.appendChild(controlBarRightPanel);
 
 
 
-        	// append the control bar inside the wrapper
+        	// append the control bar inside the main wrapper
         	videoWrapper.appendChild(controlBar);
 
+
+
+
+        	// apply all control button event listeners
+        	applyAllEventListener();
+
         }
+
+
+        // all the event listener is applied from this function.
+        function applyAllEventListener(){
+
+        	playButton.addEventListener("click", onPlayButtonClick);
+        	pauseButton.addEventListener("click", onPauseButtonClick);
+
+
+        }
+
 
 
         var onPlayButtonClick = function(e){
